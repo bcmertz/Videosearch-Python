4cb708ab3ca149abd74f7bab08bfbbd7f908cdd8
git push -u origin <bcmertz@email.wm.edu>
2017-04-12 14:55:35 -0700

    s

--- testServer.py
+++ testServer.py
@@ -13,8 +13,8 @@ import numpy as np
 import time
 import cv2
 import boto3
-from scikit-image import structural_similarity as ssim
-# from skimage.measure import structural_similarity as ssim
+# from scikit-image import structural_similarity as ssim
+from skimage.measure import structural_similarity as ssim
 from video import parseVideo, awsSave, arr1
 from stream import parseStream, awsSave, sendNode
 app = Flask(__name__)
