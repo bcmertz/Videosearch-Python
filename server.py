208faccf4b79238dac327f8b16c569e5833fe3ec
git push -u origin <bcmertz@email.wm.edu>
2017-05-02 16:50:20 -0700

    reconfigured for ec2

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-#old version of server that handles video uploads only
-
+#most current version of our server that can handle streams and uploaded videos
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import socketserver
 import requests
@@ -11,35 +10,46 @@ import json
 from io import BytesIO
 import math
 import numpy as np
-import s3
-import yaml
-import filename
-
-with open('s3.yaml', 'r') as fi:
-    config = yaml.load(fi)
-connection = s3.S3Connection(**config['s3'])
-storage = s3.Storage(connection)
+import time
+from skimage.measure import structural_similarity as ssim
+import cv2
+import boto3
+from video import parseVideo, awsSave, arr1
+# from stream import parseStream, awsSave, sendNode
 
 
 class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         print ("in post method")
+        #boilerplate to get the data from the node post to here
         cgitb.enable()
-
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
         videoFile = post_body.decode("utf-8")
         print('videoFile address', videoFile)
 
-        #PARSING OF VIDEO
-        parseVideo(videoFile)
-        print ("Video Parsing Complete, sending data to node server")
+        fileType = videoFile[0]
+        if fileType == 's':
+            print('upload type : stream')
+            videoFile = videoFile[1:len(videoFile)]
+            parseStream(videoFile)
+
+        if fileType == 'f':  #if the type is file
+            print('upload type : file')
+            videoFile = videoFile[1:len(videoFile)]  #the f was appended to the beginning of the string here we get rid of that
+            print("videoFile:", videoFile)
+            parseVideo(videoFile)  #go frame by frame and save relevant frames
+            print ("Video Parsing Complete, sending data to node server")
+
         #POST BACK TO NODE SERVER THE LINKS FROM AWS
         payload = {
-            'source': arr1
+        'source': arr1,
+        'url': videoFile
         }
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         res = requests.post('http://localhost:3000/predict', headers=headers, data=json.dumps(payload))
+        del arr1[0:len(arr1)]
+        print('Cleared data, ready to go again')
         return
 
 def run(server_class=HTTPServer, handler_class=Handler, port=8080):
