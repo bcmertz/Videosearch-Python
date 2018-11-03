24196e6c62e8c64c7fd34ece33fe91c70038a940
thistle <bcmertz@email.wm.edu>
2018-11-03 13:40:52 -0400

    updated setup

--- setup.sh
+++ setup.sh
@@ -32,24 +32,24 @@ yum -y install \
     eigen3-devel \
     && echo all done!
 
-if test ! -f /tmp/opencv.zip
-then
-    curl -LSso /tmp/opencv.zip https://github.com/opencv/opencv/archive/3.4.3.zip
-    cd /tmp
-    unzip opencv.zip
-fi
-cd /tmp/opencv-3.4.3
-mkdir -p build
-cd build
-cmake \
-    -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    ..
-make
-make install
+#if test ! -f /tmp/opencv.zip
+#then
+#    curl -LSso /tmp/opencv.zip https://github.com/opencv/opencv/archive/3.4.3.zip
+#    cd /tmp
+#    unzip opencv.zip
+#fi
+#cd /tmp/opencv-3.4.3
+#mkdir -p build
+#cd build
+#cmake \
+#    -D CMAKE_BUILD_TYPE=RELEASE \
+#    -D CMAKE_INSTALL_PREFIX=/usr/local \
+#    ..
+#make
+#make install
 echo 'export PYTHONPATH=/usr/local/lib/python3.4/site-packages' > /etc/profile.d/opencv.sh
 
 pip3 install --upgrade pip
 
-cd ../..
+#cd ../..
 pip3 install -r requirements.txt
